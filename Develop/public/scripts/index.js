48447fd6a28044e988f7f80e3745d0a386fa1261
Emma <eeg4@uw.edu>
2022-03-24 23:12:49 -0700

    Project is finished

--- Develop/public/scripts/index.js
+++ Develop/public/scripts/index.js
@@ -123,12 +123,20 @@ const showErrors = (errorObj) => {
 
 // Helper function to send a POST request to the diagnostics route (/api/diagnostics)
 const submitDiagnostics = (submissionObj) => {
-  // TODO: your code here
-  console.info(
-    '⚠️ Create the logic for the fetch POST request in scripts/index.js'
-  );
-  alert('Add your logic to scripts/index.js');
-};
+  fetch('/api/diagnostics', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(submissionObj),
+  })
+    .then((response) => response.json())
+    .then(()=>showErrors(submissionObj.errors))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  }
 
 // Function to handle when a user submits the feedback form
 const handleFormSubmit = (e) => {
@@ -157,3 +165,4 @@ const handleFormSubmit = (e) => {
 
 // Listen for when the form is submitted
 tipForm.addEventListener('submit', handleFormSubmit);
+
